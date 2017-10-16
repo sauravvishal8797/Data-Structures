226ae4332569e94b5f6836c0d2e7fa166f07d3fc
prash98 <prashant.iyer98@gmail.com>
2017-10-16 13:42:07 +0530

    graph added

---
+++ graphs/Graphs/src/graphs/Test.java
@@ -0,0 +1,12 @@
+package graphs;
+
+public class Test {
+	public static void main(String[] args){
+		int v= 5;
+		int e= 7;
+		System.out.println("Enter the edges");
+		Graphs g = new Graphs();
+		g.matrix(v,e);
+	}
+
+}
