ccda6225aec5e32bc668d692d3bdf5d57cd9de91
Rammanoj <rammanojpotla1608@gmail.com>
2017-10-16 21:26:20 +0530

    Added the File with all Binary Search Tree operations

---
+++ Node.java
@@ -0,0 +1,19 @@
+public class Node
+{
+	public int data;
+	public Node left;
+	public Node right;
+
+	public Node()
+	{
+		this.data = -1;
+		this.left = null;
+		this.right = null;
+	}
+	public Node(int n)
+	{
+		this.data = n;
+		this.left = null;
+		this.right = null;
+	}
+}
