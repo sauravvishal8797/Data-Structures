556cd31ec9c50bccfadca05cb177f3bf0c1c7434
P.V.Surya Chaitanya <psuryachaitanya@gmail.com>
2017-10-17 15:22:06 +0530

    Implemented a singly linked list

---
+++ Node.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+public class Node  {
+	public int data;
+	public Node next;
+	public Node() {
+		data=0;
+		next=null;
+	}
+	public Node (int x)
+	{
+		data=x;
+		next=null;
+	}
+	
+
+}
