ee4f30c42ab6a2b5ba24712d7cc805a53618e29c
saranyanukala <saranya.nukala1@gmail.com>
2017-10-24 16:30:24 +0530

    Added Binary Search Tree Algorithm

---
+++ BinarySearchTree/BSTNode.java
@@ -0,0 +1,18 @@
+public class BSTNode
+{
+	int data;
+	BSTNode left=null;
+	BSTNode right = null;
+	public BSTNode()
+	{
+		data=0;
+		left=null;
+		right=null;
+	}
+	public BSTNode(int n)
+	{
+		data=n;
+		left=null;
+		right=null;
+	}
+}
